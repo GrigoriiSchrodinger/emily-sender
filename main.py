71235a0e17d8eb05999e6d7097197ff6e9c0583e
Scrodinger <schrodinger.grigorii@gmail.com>
2024-12-15 19:16:03 +0300

    fix bag

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from src.feature.request.RequestHandler import RequestDataBase
 from src.logger import logger
 
 def random_seconds():
-    min_seconds = 40 * 60
-    max_seconds = 60 * 60
+    min_seconds = 30 * 60
+    max_seconds = 50 * 60
     return random.randint(min_seconds, max_seconds)
 
 def main():
@@ -19,13 +19,30 @@ def main():
     last_news_queue = request_db.get_last_news_queue()
     last_news_send = request_db.get_last_news_send()
 
+    last_news_send_seeds = {news.seed for news in last_news_send.send}
+    last_news_queue_seeds = {news.seed for news in last_news_queue.queue}
+
     attempts = 0
     while attempts < 3:
+        print(last_news_queue)
         post = gpt.choosing_post(send=last_news_send, queue=last_news_queue)
         print(post)
-        json_post = json.loads(post)
+
+        if not post:
+            print("Получен пустой ответ от gpt.choosing_post()")
+            attempts += 1
+            continue
+
+        try:
+            json_post = json.loads(post)
+        except json.JSONDecodeError as e:
+            print(f"Ошибка декодирования JSON: {e}")
+            attempts += 1
+            continue
+
         seed = json_post.get("seed")
-        if seed and all(seed != news.seed for news in last_news_send.send):
+
+        if seed and seed not in last_news_send_seeds and seed in last_news_queue_seeds:
             detail_by_seed = request_db.get_detail_by_seed(seed)
             detail_by_seed_json = {
                 "content": detail_by_seed.content,
